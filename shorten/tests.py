f2428206fb1f2310d3d9061b05ad0feb9364e7b5
kistolbov <ki.stolbov@yandx.ru>
2020-03-22 14:01:16 +0500

    add tests

--- shorten/tests.py
+++ shorten/tests.py
@@ -1,12 +1,17 @@
 from django.test import TestCase
 
+from shorten.models import URL
+from shorten.views import redirect_short_url, get_urls
+
 
 class URLShortenerTest(TestCase):
     def check_duplication(self):
-        pass
+        link = URL(url='https://www.google.com/', short_id='cutiepie')
+        url = 'https://www.google.com/'
+        self.assertEqual(link.url, url)
 
     def check_redirect_original_url(self):
-        pass
+        redirect_short_url(request='POST', short_id='cutiepie')
 
     def check_get_url(self):
-        pass
+        get_urls(request='POST')
